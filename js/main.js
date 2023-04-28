888e5b48710607e8d174adf0d2735e676b1eeaef
enudone <129606764+enudone@users.noreply.github.com>
2023-04-29 02:49:17 +0900

    先行メロをC60の1音に変更

--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
-const TWINKLE_TWINKLE = {
+const ONE_NOTE = {
   notes: [
-    { pitch: 60, startTime: 0.0, endTime: 0.5 },
-    { pitch: 60, startTime: 0.5, endTime: 1.0 },
-    { pitch: 67, startTime: 1.0, endTime: 1.5 },
-    { pitch: 67, startTime: 1.5, endTime: 2.0 },
-    { pitch: 69, startTime: 2.0, endTime: 2.5 },
-    { pitch: 69, startTime: 2.5, endTime: 3.0 },
-    { pitch: 67, startTime: 3.0, endTime: 4.0 },
-    { pitch: 65, startTime: 4.0, endTime: 4.5 },
-    { pitch: 65, startTime: 4.5, endTime: 5.0 },
-    { pitch: 64, startTime: 5.0, endTime: 5.5 },
-    { pitch: 64, startTime: 5.5, endTime: 6.0 },
-    { pitch: 62, startTime: 6.0, endTime: 6.5 },
-    { pitch: 62, startTime: 6.5, endTime: 7.0 },
-    { pitch: 60, startTime: 7.0, endTime: 8.0 },
+    { pitch: 60, startTime: 0.0, endTime: 1.0 },
   ],
-  totalTime: 8
+  totalTime: 1.0
 };
 
 // MusicRNN のモデルの初期化
@@ -45,7 +32,7 @@ function generate() {
   }
 
   // モデルはクオンタイズされたシーケンスを受け取るため、クオンタイズを実行
-  const qns = mm.sequences.quantizeNoteSequence(TWINKLE_TWINKLE, 4);
+  const qns = mm.sequences.quantizeNoteSequence(ONE_NOTE, 4);
 
   music_rnn
     .continueSequence(qns, rnn_steps, rnn_temperature)
